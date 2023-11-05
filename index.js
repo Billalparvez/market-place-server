b3f2dd479bbf73361db1230e29cb1d9ca23ce5bd
billal parvez <billalparvez80@gmail.com>
2023-11-05 12:17:40 +0600

    install mongodb,cors,express etc

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT || 5000
+app.use(cors())
+
+
+// online-marketPlace
+// uKK3YKit57VIwdd6
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.ktupw59.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const categoryCollection=client.db('online-marketDB').collection('category')
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    app.post('/category',(req,res)=>{
+        const category=req.body;
+        console.log(category)
+        res.send(category)
+    })
+    app.get('/category',async (req,res)=>{
+        const cursor=categoryCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World bangla!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
