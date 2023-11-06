f9a7e14adfc82324a1516230b92c52758e865757
billal parvez <billalparvez80@gmail.com>
2023-11-06 17:22:51 +0600

    add job data post mongodb success

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 5000
 app.use(cors())
+app.use(express.json())
 
 
 // online-marketPlace
@@ -25,12 +26,24 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const categoryCollection=client.db('online-marketDB').collection('category')
+    const addJobCollection=client.db('online-marketDB').collection('addJob')
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     app.post('/category',(req,res)=>{
         const category=req.body;
         console.log(category)
         res.send(category)
+    })
+    app.post('/addJob',async(req,res)=>{
+      const addJob=req.body
+      console.log(addJob)
+      const result=await addJobCollection.insertOne(addJob)
+      res.send(result)
+    })
+    app.get('/addJob',async (req,res)=>{
+        const cursor=categoryCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
     })
     app.get('/category',async (req,res)=>{
         const cursor=categoryCollection.find()
