5794018689c8cf95b4522fccfdac8490aac38bc5
billal parvez <billalparvez80@gmail.com>
2023-11-07 23:46:47 +0600

    bids data get api success

--- index.js
+++ index.js
@@ -59,12 +59,12 @@ async function run() {
         const result=await cursor.toArray()
         res.send(result)
     })
-    app.delete('/myBids/:id',async(req,res)=>{
-      const id=req.params.id
-      const query={_id: new ObjectId(id)}
-      const result=await bidsCollection.deleteOne(query)
-      res.send(result)
-    })
+    // app.delete('/myBids/:id',async(req,res)=>{
+    //   const id=req.params.id
+    //   const query={_id: new ObjectId(id)}
+    //   const result=await bidsCollection.deleteOne(query)
+    //   res.send(result)
+    // })
    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
